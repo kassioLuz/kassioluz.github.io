419d3cc48828d30429452364e3b42a7b1ce4d8f0
kassio luz <kassio.developer@gmail.com>
2016-04-12 11:33:48 -0400

    padrao mvc tela de login e cadastro

---
+++ view/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
+	
+	<title>Kássio Luz</title>
+
+	<!--<link rel="shortcut icon" href="assets/images/gt_favicon.png">-->
+	
+	<!-- Bootstrap -->
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+	<!-- Icon font -->
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<!-- Fonts -->
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,700">
+	<!-- Custom styles -->
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+	<!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
+</head>
+<body>
+
+<header id="header">
+	<div id="head" class="parallax" parallax-speed="1">
+		<h1 id="logo" class="text-center">
+			<img class="img-circle" src="assets/images/space_black.jpg" alt="">
+			<span class="title">Space Tech</span>
+			<span class="tagline">Essa ideia vai decolar</span>
+		</h1>
+	</div>
+
+	<nav class="navbar navbar-default navbar-sticky">
+		<div class="container-fluid">
+			
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			</div>
+			
+			<div class="navbar-collapse collapse">
+				
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="list_post.php">Home</a></li>
+					<li><a href="#">Feed</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Conteúdo <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Mobile</a></li>
+							<li><a href="#">Web</a></li>
+							<li><a href="#">Hardware</a></li>
+						</ul>
+					</li>	
+					<li><a href="#">Material</a></li>
+					<li><a href="login.php">login</a></li>
+				     <!-- Modal -->
+						<!--div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+						  <div class="modal-dialog" role="document">
+						    <div class="modal-content">
+						      <div class="modal-header">
+						        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						        <h4 class="modal-title" id="myModalLabel">Login</h4>
+						      </div>
+						      <div class="modal-body">
+						        ...
+						      </div>
+						      <div class="modal-footer">
+						        <button type="button" class="btn btn-default" data-dismiss="modal">fechar</button>
+						        <button type="button" class="btn btn-primary">Login</button>
+						      </div>
+						    </div>
+						  </div>
+						</div!-->
+			
+				</ul>
+
+			</div><!--/.nav-collapse -->			
+		</div>	
+	</nav>
+</header>
